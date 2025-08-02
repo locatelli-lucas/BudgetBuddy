e0bd644eba76e04c2456a1a143eb8ee70f194d4e
locatelli-lucas <lucas_locatelli2002@hotmail.com>
2025-08-02 15:46:50 -0300

    Finished input money formating

---
+++ Front-end/BudgetBuddy/src/components/Title.tsx
@@ -0,0 +1,11 @@
+import Image from "../assets/images/Title.png";
+import {GlobalTitle} from "../global_styles/style.ts";
+
+export function Title() {
+    return (
+        <>
+            <img src={Image} alt="Logo" />
+            <GlobalTitle>Budget Buddy</GlobalTitle>
+        </>
+    );
+}
